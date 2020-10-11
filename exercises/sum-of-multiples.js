4893edb336f27c4d563fe80f35f4312a9c9ec1ac
Emilie Anglade <anglade.em@gmail.com>
2020-10-11 21:33:50 +0200

    sum of multiples

--- exercises/sum-of-multiples.js
+++ exercises/sum-of-multiples.js
@@ -9,6 +9,19 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 
 // TODO your code:
 
+const sum = (number) => {
+    let result = 0;
+
+    for(let i = 0; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            result += i
+        }
+
+    }
+    return result
+
+}
+
 // Begin of tests
 const assert = require("assert");
 
