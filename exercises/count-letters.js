d14c5733ac26ff4d0fe42d18da308f249a4ae733
emilie-anglade <64958147+emilie-fr@users.noreply.github.com>
2020-10-14 22:06:30 +0200

    count-letters ok

--- exercises/count-letters.js
+++ exercises/count-letters.js
@@ -18,21 +18,21 @@ Add you own tests.
 
 // TODO your code:
 
-const countChar = (string, character) => {
-    if ((string === null)||(character > 1)) {
-        return -1
+const countChar = (string, letter) => {
+    if ((string === null) || (letter === null)) {
+        return -1;
+    }
+
+    else if (letter.length !==1) {
+        return -1;
     }
 
     else {
-        let countLetter = 0;
-        for (let i = 0; i < string.length; i++) {
-            if (i === character) {
-                countLetter += 1
-            }
-        }
-        return countLetter;
+           let countLetter= string.split(letter).length -1;
+           return countLetter;
     }
 }
+
 
 // Begin of tests
 const assert = require("assert");
@@ -40,9 +40,9 @@ const assert = require("assert");
 assert.strictEqual(typeof countChar, "function");
 assert.strictEqual(countChar.length, 2);
 // TODO add your tests:
-assert.strictEqual(countChar(null, a), -1);
-assert.strictEqual(countChar("", a), 0);
-assert.strictEqual(countChar(hello, h), 1);
-assert.strictEqual(countChar(abracadabra, a), 5);
-assert.strictEqual(countChar(abracadabra, b), 2);
+assert.strictEqual(countChar(null, "a"), -1);
+assert.strictEqual(countChar("", "a"), 0);
+assert.strictEqual(countChar("hello", "h"), 1);
+assert.strictEqual(countChar("abracadabra", "a"), 5);
+assert.strictEqual(countChar("abracadabra", "b"), 2);
 // End of tests
